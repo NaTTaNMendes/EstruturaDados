f9ed345845083697b398eed1b1558d5cae30f592
Nattan <nattan078@gmail.com>
2023-03-08 10:43:53 -0300

    Adicionando numero 3

--- exercicios.py
+++ exercicios.py
@@ -84,31 +84,55 @@ def mergeSort(vetor, l, r):
         merge(vetor, l, m, r)
     return vetor
 
+def testaEPrinta(vetor, fim):
+    vetorBubble = vetor
+    vetorQuick = vetor
+    vetorMerge = vetor
+    
+    inicial = time.time()
+    print(bubbleSort(vetorBubble))
+    final = time.time()
+    print('tempo bubble: ', final-inicial)
+    print()
+    
+    inicial = time.time()
+    print(quickSort(vetorQuick, 0, fim))
+    final = time.time()
+    print('tempo quick: ', final-inicial)
+    print()
+    
+    inicial = time.time()
+    print(mergeSort(vetorMerge, 0, fim))
+    final = time.time()
+    print('tempo merge: ', final-inicial)
+    print()  
+
 def main():
     vetorzinho = criaVetorEmbaralhado(10)
     vetorPequeno = criaVetorEmbaralhado(100)
     vetorMedio = criaVetorEmbaralhado(1000)
     vetorGrande = criaVetorEmbaralhado(10000)
 
-    print('Vetor pequeno:\n')
+    print('Vetorzinho:\n')
     print(vetorzinho)
+    testaEPrinta(vetorzinho, 9)
     
-    inicial = time.time()
-    print(bubbleSort(vetorzinho))
-    final = time.time()
-    print('tempo bubble: ', final-inicial)
-    print()
-    inicial = time.time()
-    print(quickSort(vetorzinho, 0, 9))
-    final = time.time()
-    print('tempo quick: ', final-inicial)
-    print()
-    inicial = time.time()
-    print(mergeSort(vetorzinho, 0, 9))
-    final = time.time()
-    print('tempo merge: ', final-inicial)
-    print()
-
+    print('Vetor pequeno:\n')
+    print(vetorPequeno)
+    testaEPrinta(vetorPequeno, 99)
+    
+    print('Vetor medio:\n')
+    print(vetorMedio)
+    testaEPrinta(vetorMedio, 999)
+    
+    print('Vetor grande:\n')
+    print(vetorGrande)
+    testaEPrinta(vetorGrande, 9999)
+    
+    
+    # A FUNÇÃO DE TESTAEPRINTA ESTÁ BUGADA, ENTRETANTO AS FUNÇÕES DE MERGE E QUICKSORT ESTÃO FUNCIONANDO PARA VALORES MUITO GRANDES.
+    # BASTA UTILIZA-LAS DE FORMA DIRETA, NO CASO DESSE FONTE NÃO ESTOU CHAMANDO-AS DIRETAMENTE POR CONTA DO EXERCICIO 3 
+    
 if __name__=="__main__":
     main()
         
